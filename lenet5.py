e83d4290872e28a985eddeb9a61fd2816b872405
night18 <warrior.sp@gmail.com>
2017-10-31 23:50:18 -0400

    finish the C5, TODO: datasaver and F6 and output

--- lenet5.py
+++ lenet5.py
@@ -14,9 +14,9 @@ def randomKernel(size):
 
 #use one kernel to whole image
 def applyKernel(source, kernel):
-	k_len = int(sqrt(len(kernel)))	#5 #5
-	s_len = int(sqrt(len(source)))	#28#12
-	run_len = int(s_len - k_len + k_len%2 )	#24#8
+	k_len = int(sqrt(len(kernel)))	#5 #5 #4
+	s_len = int(sqrt(len(source)))	#28#12#4
+	run_len = max(int(s_len - k_len + k_len%2 ), 1)	#24#8#1
 	maps = zeros(run_len**2)
 
 	kernel_sum = np.sum(kernel)
@@ -45,10 +45,9 @@ def conv(source, kernels, source_size, kernel_size):
 
 #C3 where source is a list of feature maps
 def convcube(source, kernels, source_size, kernel_size, kernel_depth):
-	feature_map = zeros((len(kernels), (source_size - kernel_size + kernel_size%2)**2 ))
+	feature_map = zeros((len(kernels), (max((source_size - kernel_size + kernel_size%2), 1))**2 ))
 	for x in xrange(len(kernels)):
 		for y in xrange(kernel_depth):
-			# TODO 1030 get the subarray or the cube kernel
 			kernel = kernels[x]
 			layer_kernel = kernel[y*(kernel_size**2): (y+1)*(kernel_size**2)]
 			feature_map[x] += applyKernel(source[y], layer_kernel)
@@ -77,12 +76,17 @@ def pool(source, s_len):
 
 	return feature_map
 
+def fileSaver(data, txt_name):
+	file = open(txt_name+".txt", "w+")
+	file.write(data)
+	file.close()
+
 #parameters
 source_size = 28
 kernel_size = 5	#recommend use odd number
 fk_size = 6		#first kernel size
 sk_size = 16	#second kernel size
-first_nodes_size = 120 #amount of first full connection layer nodes 
+trd_size = 120 	#third kernel size for C5
 second_nodes_size = 84 #amount of second full connection layer nodes
 class_size = 10 #classes number
 
@@ -90,7 +94,6 @@ class_size = 10 #classes number
 mndata = MNIST('sample')
 images, labels = mndata.load_training()
 # test_image, test_label = mndata.load_testing()
-
 
 
 #ndarray
@@ -115,12 +118,15 @@ S2 = zeros((fk_size, S2_size ** 2))
 
 second_kernel = zeros((sk_size, fk_size * kernel_size ** 2))
 for x in xrange(sk_size):
-	second_kernel[x] = randomKernel(fk_size * kernel_size ** 2 ) #the kernel are cubes 
+	second_kernel[x] = randomKernel(fk_size * kernel_size ** 2 ) #the kernel are cubes  5*5*6
 
 C3_size = int(S2_size - kernel_size + kernel_size%2) #8
 C3 = zeros((sk_size, C3_size ** 2))
 
-
+C5_kernel_size = int(C3_size/2) #4 in Lenet paper, it is using 5*5, it is also S4_size
+third_kernel = zeros(( trd_size, sk_size * C5_kernel_size ** 2))
+for x in xrange(trd_size):
+	third_kernel[x] = randomKernel(sk_size * C5_kernel_size ** 2) #the kernel are cubes 4*4*16
 
 for x in xrange(5,6):
 	
@@ -133,13 +139,21 @@ for x in xrange(5,6):
 	C3 = convcube(S2, second_kernel, S2_size, kernel_size, fk_size)
 	#S4 subsampling Layer 16@4*4
 	S4 = pool(C3, C3_size)
+	
+	#C5 Actually it is a convolutional layer 120@1*1, the kernel is 120@5*5*16
+	C5 = convcube(S4, third_kernel, C5_kernel_size, C5_kernel_size, sk_size)
 
-	#Full connection 
+	pprint(C5.shape)
 
-	# pprint(S4.shape)
+# 	for y in xrange(16):
+# 		plt.subplot(1,16,y+1)
+# 		plt.imshow(C3[y].reshape(8,8), cmap='gray')
+# 		plt.axis('off')
+# plt.show()
 
-	for y in xrange(16):
-		plt.subplot(1,16,y+1)
-		plt.imshow(C3[y].reshape(8,8), cmap='gray')
-		plt.axis('off')
-plt.show()
+# fileSaver(first_kernel,"first_kernel")
+# fileSaver(second_kernel,"second_kernel")
+# fileSaver(third_kernel,"third_kernel")
+# fileSaver(C1,"C1")
+# fileSaver(C3,"C3")
+# fileSaver(C5,"C5")
